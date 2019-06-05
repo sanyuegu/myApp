ff1b5ccc85652081ef0ab3ff27cc27dc2a14fa24
sanyuegu <sanyuegu.126.com>
2019-06-05 11:07:28 +0800

    添加css样式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'myApp';
-  names = ['gg', 'dd', 'cc'];
+  private greeting: string;
+  private firstValue: string ;
+  private secondValue: string ;
+
+  @ViewChild ('newtitle') newtitle: ElementRef;
+
+  @ViewChild ('newlink') newlink: ElementRef;
+
+  constructor() {
+    this.greeting =  'A greeting value';
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    if (title === undefined || link === undefined) {
+      console.log('title is undefined or link is undefined!');
+      return false;
+    }
+
+
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.firstValue = title.value;
+    this.secondValue = link.value;
     return false;
   }
 }
